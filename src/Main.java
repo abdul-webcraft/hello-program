fa2a1c3e9e66967422f65d3670ca78b2a7ab139f
abdul-webcraft <abdul@webcraftsolutions.in>
2024-09-03 15:43:19 +0530

    remove \n and add print to println in Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello JAVA! \n");
+        System.out.println("Hello JAVA! ");
 
         System.out.print("This is web craft solutions team.");
 
